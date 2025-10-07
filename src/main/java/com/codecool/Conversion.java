59e7a76c709cb069b79f3d060b40a64c77aff396
Tamás Morcsányi <mtomi8@gmail.com>
2025-10-07 12:01:22 +0200

    add solution

--- src/main/java/com/codecool/Conversion.java
+++ src/main/java/com/codecool/Conversion.java
@@ -13,9 +13,39 @@ public class Conversion {
         put(1000, "M");
     }};
 
-    StringBuilder getRomanSymbol(char digit, int decimalPlace) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public String solution(int number){
+        return romanNumeralsEncoder(number);
+    }
 
+    private String romanNumeralsEncoder(int number) {
+        if (number >= 4000) throw new Error("Unsupported number");
+        String digits = Integer.toString(number);
+        StringBuilder result = new StringBuilder();
+        for (int index = 0; index < digits.length(); index++) {
+            char digit = digits.charAt(index);
+            StringBuilder character = getRomanSymbol(digit, digits.length() - 1 - index);
+            result.append(character);
+        }
+        return result.toString();
+    }
+
+    StringBuilder getRomanSymbol(char digit, int decimalPlace) {
+        int intNum = Integer.parseInt(String.valueOf(digit));
+        int divByFive = intNum / 5;
+        int modByFive = intNum % 5;
+        int symbolIndex = (int)Math.pow(10, decimalPlace);
+
+        StringBuilder result = new StringBuilder();
+
+        if (modByFive == 4) {
+            return new StringBuilder(symbols.get(symbolIndex) + symbols.get(symbolIndex * 5 * (divByFive + 1)));
+        } else {
+            if (divByFive != 0) {
+                result.append(symbols.get(symbolIndex * 5));
+            }
+            result.append(symbols.get(symbolIndex).repeat(modByFive));
+        }
+        return result;
     }
 }
 
